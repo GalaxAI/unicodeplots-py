2a491431d5bd1acaad0b6c8de7c4b6fd85b49392
GalaXai <92019780+GalaXai@users.noreply.github.com>
2025-03-18 21:09:54 +0100

    feat: better __init__.py for canvas

--- unicodeplots/canvas/__init__.py
+++ unicodeplots/canvas/__init__.py
@@ -1,1 +1,10 @@
-from .canvas import Canvas, ColorType, INVALID_COLOR, Color
+from unicodeplots.canvas.canvas import Canvas, ColorType, INVALID_COLOR, Color
+from unicodeplots.canvas.braile import BrailleCanvas
+
+__all__ = [
+    'Canvas', 
+    'ColorType', 
+    'INVALID_COLOR', 
+    'Color', 
+    'BrailleCanvas'
+]
