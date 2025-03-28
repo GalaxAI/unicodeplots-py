ad452b8b34a3c2ab885401b63687bb27353c7a35
GalaXai <92019780+GalaXai@users.noreply.github.com>
2025-03-28 02:08:58 +0100

    feat: broken line into to functions, and moved SUPERSAMPLE outside of function

--- unicodeplots/canvas/braile.py
+++ unicodeplots/canvas/braile.py
@@ -22,7 +22,7 @@ class BrailleCanvas(Canvas):
             **kwargs: Additional parameters that override values in params if provided
         """
         super().__init__(params=params, **kwargs)
-
+        self.SUPERSAMPLE: int = 8
         self.default_char = 0x2800
         self.active_cells = [[self.default_char] * self.grid_cols for _ in range(self.grid_rows)]
 
@@ -56,20 +56,8 @@ class BrailleCanvas(Canvas):
         # Update color (simple overwrite)
         self.active_colors[cy][cx] = color
 
-    def line(self, x1: float, y1: float, x2: float, y2: float, color: ColorType):
-        """Draw a line between logical coordinates using supersampled Bresenham for smoother curves"""
-        # Convert to high-resolution pixel coordinates (8x supersampling)
-        # NOTE: If this really gives bad performance we can only do super sampling for curves or as a flag
-        # Or go back to d34b commit
-        SUPERSAMPLE = 8
-        px1 = self.x_to_pixel(x1) * SUPERSAMPLE
-        py1 = self.y_to_pixel(y1) * SUPERSAMPLE
-        px2 = self.x_to_pixel(x2) * SUPERSAMPLE
-        py2 = self.y_to_pixel(y2) * SUPERSAMPLE
-
-        # Standard Bresenham at high resolution
-        px1, py1 = int(round(px1)), int(round(py1))
-        px2, py2 = int(round(px2)), int(round(py2))
+    def _draw_bresenham_segment(self, px1: int, py1: int, px2: int, py2: int, color: ColorType):
+        """Draws a single line segment using Bresenham given INTEGER pixel coordinates."""
 
         dx = abs(px2 - px1)
         dy = abs(py2 - py1)
@@ -77,27 +65,40 @@ class BrailleCanvas(Canvas):
         sy = 1 if py1 < py2 else -1
         err = dx - dy
 
-        # Use a set to collect unique final pixels
         pixels = set()
+        px_curr, py_curr = px1, py1
 
         while True:
-            # Convert supersampled coordinates back to normal resolution
-            pixels.add((px1 // SUPERSAMPLE, py1 // SUPERSAMPLE))
+            pixels.add((px_curr // self.SUPERSAMPLE, py_curr // self.SUPERSAMPLE))
 
-            if px1 == px2 and py1 == py2:
+            if px_curr == px2 and py_curr == py2:
                 break
 
             e2 = 2 * err
             if e2 > -dy:
                 err -= dy
-                px1 += sx
+                px_curr += sx
             if e2 < dx:
                 err += dx
-                py1 += sy
+                py_curr += sy
 
-        # Draw all collected pixels
-        for x, y in pixels:
-            self._set_pixel(x, y, color)
+        # Set the actual pixels on the canvas
+        for p_x, p_y in pixels:
+            self._set_pixel(p_x, p_y, color)
+
+    def line(self, x1: float, y1: float, x2: float, y2: float, color: ColorType):
+        """Draw a line between logical coordinates using supersampled Bresenham for smoother curves"""
+
+        px1 = self.x_to_pixel(x1) * self.SUPERSAMPLE
+        py1 = self.y_to_pixel(y1) * self.SUPERSAMPLE
+        px2 = self.x_to_pixel(x2) * self.SUPERSAMPLE
+        py2 = self.y_to_pixel(y2) * self.SUPERSAMPLE
+
+        # Standard Bresenham at high resolution
+        px1, py1 = int(round(px1)), int(round(py1))
+        px2, py2 = int(round(px2)), int(round(py2))
+
+        self._draw_bresenham_segment(px1, py1, px2, py2, color)
 
     def render(self) -> str:
         """Efficient rendering with pre-allocated strings"""
@@ -105,24 +106,3 @@ class BrailleCanvas(Canvas):
             "".join(ColorType(self.active_colors[row][col]).apply(chr(self.active_cells[row][col])) for col in range(self.grid_cols))
             for row in range(self.grid_rows)
         )
-
-
-# Demo Script
-if __name__ == "__main__":
-    canvas = BrailleCanvas(width=32, height=16, resolution=4, origin_x=0, origin_y=0, xflip=False, yflip=False, blend=False)
-
-    points = [(0, 16), (5, 0), (32, 16)]
-
-    for i in range(len(points) - 1):
-        x1, y1 = points[i]
-        x2, y2 = points[i + 1]
-        print(f"Drawing line from {(x1, y1)} to {(x2, y2)}")
-        canvas.line(x1, y1, x2, y2, color=Color.BLUE)
-    b = [(0, 0), (5, 5), (32, 10)]
-    for i in range(len(b) - 1):
-        x1, y1 = b[i]
-        x2, y2 = b[i + 1]
-        print(f"Drawing line from {(x1, y1)} to {(x2, y2)}")
-        canvas.line(x1, y1, x2, y2, color=Color.RED)
-
-    print(canvas.render())
