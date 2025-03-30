d21593474f4a04d728c473a55c31593dd8bf4242
GalaXai <92019780+GalaXai@users.noreply.github.com>
2025-03-30 05:49:03 +0200

    feat: test for Scatter example

--- tests/test_Examples.py
+++ tests/test_Examples.py
@@ -1,4 +1,5 @@
 import math
+from random import seed, uniform
 
 from unicodeplots import Lineplot
 
@@ -23,3 +24,13 @@ def test_lineplot_example_02(snapshot):
     plot = Lineplot(x_vals, math.sin, x_vals, math.cos, width=80, height=60, show_axes=True, border="single", xlabel="x", ylabel="f(x)")
     output = plot.render()
     snapshot.assert_match(output, "Lineplot_Example_02.txt")
+
+
+def test_lineplot_example_03(snapshot):
+    seed(42)
+    x = [uniform(-5, 5) for _ in range(100)]
+    y = [uniform(-5, 5) for _ in range(100)]
+
+    plot = Lineplot(x, y, scatter=True, title="Random Scatter Plot", show_axes=True)
+    output = plot.render()
+    snapshot.assert_match(output, "Lineplot_Example_03.txt")
