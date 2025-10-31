c7cdcdd643cb7ec0a004f69e19be8ec82b5b3ae8
GalaXai <92019780+GalaXai@users.noreply.github.com>
2025-10-31 22:42:46 +0100

    feat: new TensorAdapter with first tests

--- unicodeplots/utils/tensor.py
+++ unicodeplots/utils/tensor.py
@@ -1,6 +1,5 @@
 import os
 import random
-from collections.abc import Iterable
 
 import numpy as np
 import torch
@@ -9,16 +8,34 @@ from tinygrad import Tensor as TinyTensor
 os.environ["CPU"] = "1"
 
 
-class Tensor:
-    def __init__(self, data):
-        if isinstance(data, Iterable):
-            try:
-                self.data = [Tensor(x) for x in data]
-            except TypeError:  # pytorch d0 is iterable
-                self.data = data.item()
-        else:
-            self.data = data
+def forward_op(name):
+    def op(self, other):
+        if isinstance(other, TensorAdapter):
+            other = other.data
+        result = getattr(self.data, name)(other)
+        return TensorAdapter(result)
 
+    return op
+
+
+class TensorAdapter:
+    def __init__(self, data):
+        self.data = data
+
+    def __getattr__(self, name):
+        if name not in dir(self):
+            return getattr(self.data, name)
+
+    # to support python lists
+    @staticmethod
+    def _shape(x) -> tuple:
+        if not isinstance(x, list):
+            return ()
+        if not x:
+            return (0,)
+        return (len(x), *TensorAdapter._shape(x[0]))
+
+    @property
     def shape(self) -> tuple:
         if hasattr(self.data, "shape"):
             return self.data.shape
@@ -26,29 +43,26 @@ class Tensor:
             return ()
         if not self.data:
             return (0,)
-        return (len(self.data), *self.data[0].shape())
+        return self._shape(self.data)
 
-    def __add__(self, other) -> "Tensor":
-        return Tensor(self.data + other.data)
-
-    def __sub__(self, other) -> "Tensor":
-        return Tensor(self.data - other.data)
-
-    def __mul__(self, other) -> "Tensor":
-        return Tensor(self.data * other.data)
-
-    def __truediv__(self, other) -> "Tensor":
-        return Tensor(self.data / other.data)
+    __add__ = forward_op("__add__")
+    __radd__ = forward_op("__radd__")
+    __sub__ = forward_op("__sub__")
+    __rsub__ = forward_op("__rsub__")
+    __mul__ = forward_op("__mul__")
+    __rmul__ = forward_op("__rmul__")
+    __truediv__ = forward_op("__truediv__")
+    __rtruediv__ = forward_op("__rtruediv__")
 
     def __str__(self):
         if type(self.data).__module__ == "tinygrad.tensor":
-            return f"Tensor({self.data.tolist()})"
-        return f"Tensor({self.data})"
+            return f"tinygrad.tensor({self.data.tolist()})"
+        return str(self.data)
 
     def __repr__(self):
         if type(self.data).__module__ == "tinygrad.tensor":
-            return f"Tensor({self.data.tolist()})"
-        return f"Tensor({self.data})"
+            return f"tinygrad.tensor({self.data.tolist()})"
+        return repr(self.data)
 
 
 if __name__ == "__main__":
@@ -56,21 +70,12 @@ if __name__ == "__main__":
     trch = torch.randint(0, 256, (28, 28, 3))
     nmpy = np.random.randint(0, 256, size=(28, 28, 3))
     tiny = TinyTensor.randint(28, 28, 3, low=0, high=256).tolist()
-    for _ in range(1):
-        pyt = Tensor(py)
-        trch = Tensor(trch)
-        nmpy = Tensor(nmpy)
-        tiny = Tensor(tiny)
-        print(pyt.shape(), trch.shape(), nmpy.shape(), tiny.shape())
-    # a = [1, 1, 1]
-    # b = [2, 2, 2]
-    # print("HELLO")
-    # for t in [torchTensor, np_array, TinyTensor]:
-    #     a_t = t(a)
-    #     b_t = t(b)
-    #     c_t = a_t / b_t
-    #     print(a_t, b_t, c_t)
-    #     c = Tensor(a_t) / Tensor(b_t)
-    #     print(c, c.data == c_t)
-    # print("HELLO")
-#
+    pyt = TensorAdapter(py)
+    trch = TensorAdapter(trch)
+    nmpy = TensorAdapter(nmpy)
+    tiny = TensorAdapter(tiny)
+    print(pyt.shape, trch.shape, nmpy.shape, tiny.shape)
+    a = TensorAdapter(TinyTensor([1]))
+    b = TensorAdapter(TinyTensor(1))
+    c = a + b
+    print(c, a, b)
